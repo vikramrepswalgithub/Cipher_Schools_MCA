8a4d08aff8c4fdc14cf3bbd5e6e7379a23075a5b
vikramrepswalgithub <137646960+vikramrepswalgithub@users.noreply.github.com>
2023-07-01 22:35:07 +0530

    Create first_program.cpp

---
+++ first_program.cpp
@@ -0,0 +1,17 @@
+//write our first code in c++ programme  lect -01
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int sum (int n1, int n2){
+    return n1+n2;
+}
+
+int main(){
+
+    int n1,n2;
+    cin>>n1>>n2;
+    cout<< " the sum of n1 & n2 is : "<< sum (n1 ,n2);
+
+    return 0;
+}
